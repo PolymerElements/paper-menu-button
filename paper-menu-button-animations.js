aab5257fd1ad8eeb95c4d9b8c273688f43fe9b2e
Peter Burns <rictic@google.com>
2018-10-24 12:38:21 -0700

    Explicitly set _template to null.
    This lets us skip a querySelector on initial bootup, and makes this code compatible with strictTemplatePolicy.

--- paper-menu-button-animations.js
+++ paper-menu-button-animations.js
@@ -14,6 +14,7 @@ import {NeonAnimationBehavior} from '@polymer/neon-animation/neon-animation-beha
 import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
 Polymer({
   is: 'paper-menu-grow-height-animation',
+  _template: null,
 
   behaviors: [NeonAnimationBehavior],
 
